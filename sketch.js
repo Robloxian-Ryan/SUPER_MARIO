219b0f9b7225ab2f26870b5b19d0dba1a69d15cb
Robloxian-Ryan <67097715+Robloxian-Ryan@users.noreply.github.com>
2021-04-23 20:41:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
 var marioImage, mario;
 var groundImage, ground;
 var bgImage;
-var obstacleImage, obstacle;
+var obstacleImage, obstacle, obstacleGroup;
+var brick, brickImage, brickGroup;
+var coin, coinImage, coinGroup;
+var gameState;
 
 function preload() {
     bgImage = loadImage("bg.png");
+
     groundImage = loadImage("ground2.png");
+
     marioImage = loadAnimation(
         "mario00.png",
         "mario01.png",
@@ -18,7 +23,11 @@ function preload() {
         "obstacle4.png",
         "obstacle3.png",
         "obstacle1.png"
-    );
+        );
+        
+    brickImage = loadImage("brick.png");
+
+    coinImage = loadImage("coin.png");
 }
 
 function setup() {
@@ -32,14 +41,19 @@ function setup() {
     mario.addAnimation("marioImage", marioImage);
     mario.scale = 3;
 
-    obstacle = createSprite(1400, 420, 1000, 300);
-    obstacle.addAnimation("obstacleImage", obstacleImage);
-    obstacle.scale = 2;
+    obstacleGroup = new Group();
+    brickGroup = new Group();
+    coinGroup = new Group();
+
+    gameState = "play";
 }
 
 function draw() {
     background(bgImage);
     drawSprites();
+    spawnObstacles();
+    spawnBricks();
+    spawnCoins();
 
     if(ground.x <= 0) {
         ground.x = 600;
@@ -48,8 +62,53 @@ function draw() {
     mario.velocityY += 1;
     
     if(keyDown("space") && mario.collide(ground)) {
-        mario.velocityY = -15;
+        mario.velocityY = -20;
     }
 
     mario.collide(ground);
+
+    for (var i = 0; i < brickGroup.length; i++) {
+        if(brickGroup.get(i).isTouching(mario)) {
+            brickGroup.get(i).remove();
+            mario.velocityY += 5;
+        }
+    }
+
+    for (var i = 0; i < coinGroup.length; i++) {
+        if(coinGroup.get(i).isTouching(mario)) {
+            coinGroup.get(i).remove();
+        }
+    }
+
+}
+
+function spawnObstacles() {
+    if(frameCount % 100 === 0) {
+        obstacle = createSprite(1210, 420, 1000, 300);
+        obstacle.addAnimation("obstacleImage", obstacleImage);
+        obstacle.scale = 2;
+        obstacle.velocityX = ground.velocityX;
+        obstacleGroup.add(obstacle);
+    }
+}
+
+function spawnBricks() {
+    var rand = Math.round(random(120, 170));
+    if(frameCount % rand === 0) {
+        brick = createSprite(1210, 300, 1000, 300);
+        brick.addImage("brickImage", brickImage);
+        brick.scale = 2;
+        brick.velocityX = ground.velocityX;
+        brickGroup.add(brick);
+    }
+}
+
+function spawnCoins() {
+    if(frameCount % 155 === 0) {
+        coin = createSprite(1210, 330, 1000, 300);
+        coin.addImage("coinImage", coinImage);
+        coin.scale = 1;
+        coin.velocityX = ground.velocityX;
+        coinGroup.add(coin);
+    }
 }
